a48e870cfe0b61f7942e6defd4b0a325bda77dfa
Alan Morales <alanmg@fb.com>
2015-03-02 17:22:57 -0800

    Cloud code call fixes

--- AMGParseSampleSource.m
+++ AMGParseSampleSource.m
@@ -564,8 +564,12 @@ bool pinned_first = NO;
                 PFObject *objectWithPointer = (PFObject *)object;
                 
                 NSLog(@"randomColumn Value %@", objectWithPointer[@"randomColumn"]);
-                PFObject *aclTest = objectWithPointer[@"aclTestObject"];
-                NSLog(@"aclTest isDataAvailable %@", aclTest.isDataAvailable);
+                PFObject *aclTest = objectWithPointer[@"pointer"];
+                NSLog(@"%@", aclTest);
+                NSLog(@"Linked ACLTest objectID %@", aclTest.objectId);
+                [aclTest fetchIfNeededInBackgroundWithBlock:^(PFObject *object, NSError *error) {
+                    NSLog(@"fetch in background if needed! %@", object[@"value"]);
+                }];
             }];
             break;
         }
